0e2c4c99b472be72b21ebc08b6e2e028507b8214
shiveshnavin <shiveshnavin@gmail.com>
2018-07-03 19:54:39 +0530

    AA

--- src/lib_mos.c
+++ src/lib_mos.c
@@ -13,7 +13,7 @@ bool mgos_lib_mos_init(void) {
 
   printf("Hello From Library");
 
-  LOG(LL_INFO, ("%s", "Hello From Library Log"));  
+  LOG(LL_INFO, ("%s", "Hello From Library Log"));  /*
   mgos_gpio_set_mode(4, MGOS_GPIO_MODE_OUTPUT);
   mgos_gpio_set_mode(16, MGOS_GPIO_MODE_OUTPUT);
   mgos_gpio_set_mode(5, MGOS_GPIO_MODE_OUTPUT);
@@ -22,6 +22,13 @@ bool mgos_lib_mos_init(void) {
   mgos_pwm_set(4,0.8,200);
   mgos_pwm_set(16,0.8,200);
   mgos_pwm_set(5,0.8,200);
-  mgos_pwm_set(19,0.8,200);
+  mgos_pwm_set(19,0.8,200);*/
+
+ struct mgos_pwm_rgb_led led;
+ mgos_pwm_rgb_led_init(&led, 4, 16, 5);
+ mgos_pwm_rgb_led_set(&led, 255, 255, 255, 250); 
+
+
+
   return true;
 }
