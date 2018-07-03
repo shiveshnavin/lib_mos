4506f787d3e47489938951c55abef245cb53857a
shiveshnavin <shiveshnavin@gmail.com>
2018-07-03 19:48:44 +0530

    Added GOIO

--- src/lib_mos.c
+++ src/lib_mos.c
@@ -13,7 +13,15 @@ bool mgos_lib_mos_init(void) {
 
   printf("Hello From Library");
 
-  LOG(LL_INFO, ("%s", "Hello From Library"));
-  mgos_pwm_set(4,0.4,200);
+  LOG(LL_INFO, ("%s", "Hello From Library Log"));  
+  mgos_gpio_set_mode(4, MGOS_GPIO_MODE_OUTPUT);
+  mgos_gpio_set_mode(16, MGOS_GPIO_MODE_OUTPUT);
+  mgos_gpio_set_mode(5, MGOS_GPIO_MODE_OUTPUT);
+  mgos_gpio_set_mode(19, MGOS_GPIO_MODE_OUTPUT);
+
+  mgos_pwm_set(4,0.8,200);
+  mgos_pwm_set(16,0.8,200);
+  mgos_pwm_set(5,0.8,200);
+  mgos_pwm_set(19,0.8,200);
   return true;
 }
