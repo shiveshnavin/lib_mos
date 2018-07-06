dcd7f92732bc519b5975f6658716486e870fc03c
shiveshnavin <shiveshnavin@gmail.com>
2018-07-06 20:12:13 +0530

    Added RGB

--- src/lib_mos.c
+++ src/lib_mos.c
@@ -7,8 +7,24 @@ bool is2(int no)
 	return no==2;
 
 }
+void setrgbw(float r,float g,float b,float w){
+
+  mgos_pwm_set(4,200,r);
+  mgos_pwm_set(16,200,g);
+  mgos_pwm_set(5,200,b);
+  mgos_pwm_set(19,200,w);
+
+}
 
 
+bool is_firmware_loaded()
+{
+//todo
+return false;
+}
+
+int RED=1,GREEN=2,BLUE=3;
+int curColor=RED;
 bool mgos_lib_mos_init(void) {
 
   printf("Hello From Library");
@@ -19,10 +35,41 @@ bool mgos_lib_mos_init(void) {
   mgos_gpio_set_mode(5, MGOS_GPIO_MODE_OUTPUT);
   mgos_gpio_set_mode(19, MGOS_GPIO_MODE_OUTPUT);
 
-  mgos_pwm_set(4,200,1);
-  mgos_pwm_set(16,200,1);
-  mgos_pwm_set(5,200,0);
-  mgos_pwm_set(19,200,1);
+
+//in timer loop
+	if(is_firmware_loaded()){
+		
+		if(curColor==RED)
+		{
+
+		curColor=GREEN;
+		setrgbw(0,1,0,0);
+
+		}
+		else if(curColor==GREEN)
+		{
+
+		curColor=BLUE;
+		setrgbw(0,0,1,0);
+
+		}
+		else if(curColor==BLUE{
+
+
+		curColor=RED;
+		setrgbw(1,0,0,0);
+
+		}
+		else{
+
+		curColor=RED;
+		setrgbw(1,0,0,0);
+
+		}
+
+	}
+ 
+ 	
 /*
  struct mgos_pwm_rgb_led led;
  mgos_pwm_rgb_led_init(&led, 4, 16, 5);
