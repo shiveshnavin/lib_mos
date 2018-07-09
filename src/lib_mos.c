baf615f42858fc4956709590d70831342467300c
shiveshnavin <shiveshnavin@gmail.com>
2018-07-09 20:58:26 +0530

    Added FFI

--- src/lib_mos.c
+++ src/lib_mos.c
@@ -308,7 +308,7 @@ json_prettify_file("loading.json");
 
 
 
-/*
+
 
 	if(mgos_sys_config_get_wifi_sta_enable() || mgos_sys_config_get_wifi_sta1_enable() || mgos_sys_config_get_wifi_sta2_enable())
 	{	
@@ -321,7 +321,7 @@ json_prettify_file("loading.json");
 				LOG(LL_INFO, ("%s", mgos_sys_config_get_wifi_sta_ssid()));
   
 				
-				setPrevColor();
+				//setPrevColor();
 			}
 			else{
 				LOG(LL_INFO, ("%s", "lib_mos:wifi ssid is null"));
@@ -331,7 +331,7 @@ json_prettify_file("loading.json");
 	else{
 				LOG(LL_INFO, ("%s", "lib_mos:wifi is off"));
 				blinkb();
-			}*/
+			}/**/
 	
 	
 	return true;
